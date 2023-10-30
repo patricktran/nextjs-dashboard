50613ccbe4ae43f2e3f544665c8d3741676fbea6
patrick.tran <patrick.tran@abbvie.com>
2023-10-30 14:17:52 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+//This layout is required and is called a root layout. Any UI you add to the root layout will be shared across all pages in your application.
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
